26884a80d60e11b1c10c59d6de6d727eaf2c2d07
ddmills <dmills@iastate.edu>
2015-11-29 22:22:38 -0600

    added jQuery

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -1,4 +1,5 @@
 import Task from './Task';
+import $ from './jquery';
 
 var t1 = new Task(0, 2, 1);
 var t2 = new Task(0, 4, 1);
@@ -7,3 +8,7 @@ var t3 = new Task(0, 7, 1);
 console.log(t1.toString());
 console.log(t2.toString());
 console.log(t3.toString());
+
+$(document).on('click', '#btn-add-task', function() {
+  console.log('add task');
+})
