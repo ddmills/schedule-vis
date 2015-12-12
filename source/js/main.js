97ba4ea8362327e16a84d9f77d0b4142b5dba682
ddmills <dmills@iastate.edu>
2015-12-12 17:44:58 -0600

    removed some logs

--- source/js/main.js
+++ source/js/main.js
@@ -9,19 +9,6 @@ var tasks = new TaskSet();
 var rms = new RMS();
 var edf = new EDF();
 
-tasks.addTask(new Task(0, 2, 1));
-
-console.log(rms.check(tasks));
-console.log(edf.check(tasks));
-
-var t1 = new Task(0, 2, 1);
-var t2 = new Task(0, 4, 1);
-var t3 = new Task(0, 7, 1);
-
-console.log(t1.toString());
-console.log(t2.toString());
-console.log(t3.toString());
-
 var inputStart = $('#add-task-start');
 var inputPeriod = $('#add-task-period');
 var inputDuration = $('#add-task-duration');
@@ -55,7 +42,10 @@ $(document).on('click', '#btn-add-task', function() {
 
   if (!error) {
     var task = new Task(start, period, duration);
-    tasks.add(task);
+    tasks.addTask(task);
+
+    console.log(rms.check(tasks));
+    console.log(edf.check(tasks));
   }
 
 });
